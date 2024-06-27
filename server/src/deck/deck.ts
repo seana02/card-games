311a7358a34ee39f656186438849b6484dc57b8a
seana02 <seanseptember2002@gmail.com>
2024-06-26 21:38:46 -0400

    basic deck functions

--- server/src/deck/deck.ts
+++ server/src/deck/deck.ts
@@ -7,16 +7,24 @@ class Card {
     constructor(s: Suit, v: number) {
         this._suit = s;
         if (v < 1 || v > 14) {
-            throw Error;
+            throw Error("Invalid card value");
         }
         this._value = v;
     }
+
+    public get suit() { return this._suit; }
+    public get val() { return this._value; }
 }
 
 class Deck {
     private _deck: Card[];
 
-    constructor() {
+    constructor(init = false) {
+        this._deck = [];
+        if (init) this.init();
+    }
+
+    public init() {
         for (let i = 1; i <= 13; i++) {
             this._deck.push(new Card(Suit.Club, i));
             this._deck.push(new Card(Suit.Heart, i));
@@ -32,6 +40,22 @@ class Deck {
             this._deck[i] = this._deck[choice];
             this._deck[choice] = temp;
         }
+    }
+
+    public peek(): Card | null {
+        return this._deck[this._deck.length - 1];
+    }
+
+    public draw(): Card | null {
+        return this._deck.pop();
+    }
+
+    public add(c: Card) {
+        this._deck.push(c);
+    }
+
+    public addDeck(d: Deck) {
+        this._deck.push(...d._deck);
     }
 
 }
