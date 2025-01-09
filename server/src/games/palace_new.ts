b5cfc64af70d759b1eede74b28c637144cd9ac06
seana02 <seanseptember2002@gmail.com>
2025-01-09 03:25:45 -0500

    made hidden cards display even if it is a bomb

--- server/src/games/palace_new.ts
+++ server/src/games/palace_new.ts
@@ -229,18 +229,23 @@ export class GameState {
         let cardVal = player.hidden[index].value;
         newState.centerPile.add(player.hidden[index]);
         newState.lastPlayed = [player.hidden[index]];
-        player.hidden[index] = null;
 
         // card already moved to center pile
         if (checkCompletes(this.centerPile, cardVal, 0)) {
             newState.centerPile.clear();
+            player.hidden[index] = null;
             return newState;
         } 
 
         if (checkPlayable(newState.centerPile)) {
-            applyEffectsHelper(newState, newState.centerPile.peek(1).value, 1);
+            let value = newState.centerPile.peek(1).value;
+            applyEffectsHelper(newState, value, 1);
+            if (value === 3 || value === 10) {
+                newState.lastPlayed = [player.hidden[index]];
+            }
         }
 
+        player.hidden[index] = null;
         return newState;
     }
 
